7c8b7e406fe2536dc3f5e57b75a62b9cdcf5bc88
tetov <anton@tetov.se>
2020-05-12 10:51:35 +0200

    Working kernel compile setup

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM dockcross/linux-arm64
-
-ENV DEFAULT_DOCKCROSS_IMAGE arm64-kernel-xcompile
-RUN apt-get install libssl-dev fakeroot
--- get-src.sh
+++ get-src.sh
@@ -4,11 +4,10 @@ set -e
 # Find kernel version with a matching rt patch
 MAJOR="5"
 MINOR="4"
-PATCH="34"
+PATCH="39"
 VERSION="$MAJOR.$MINOR.$PATCH"
 
-RT_PATCH_VER="rt21"
-
+RT_PATCH_VER="rt23"
 
 # Find a rpi conf for kernel version (bcm2771 for RPi4)
 CONF_URL="https://raw.githubusercontent.com/raspberrypi/linux/rpi-5.4.y/arch/arm64/configs/bcm2711_defconfig"
@@ -19,7 +18,6 @@ KERNEL_SRC_SIGN_URL="https://cdn.kernel.org/pub/linux/kernel/v${MAJOR}.x/linux-$
 PATCH_URL="https://cdn.kernel.org/pub/linux/kernel/projects/rt/${MAJOR}.${MINOR}/patch-${VERSION}-${RT_PATCH_VER}.patch.xz"
 PATCH_URL_SIGN="https://cdn.kernel.org/pub/linux/kernel/projects/rt/${MAJOR}.${MINOR}/patch-${VERSION}-${RT_PATCH_VER}.patch.sign"
 
-
 wget -N "$PATCH_URL"
 wget -N "$PATCH_URL_SIGN"
 
@@ -28,23 +26,24 @@ wget -N "$KERNEL_SRC_SIGN_URL"
 
 wget -O .config "$CONF_URL"
 
-xz -dkfv linux-${VERSION}.tar.xz
-xz -dkfv patch-${VERSION}-${RT_PATCH_VER}.patch.xz
+xz -dkfv linux-"${VERSION}".tar.xz
+xz -dkfv patch-"${VERSION}"-"${RT_PATCH_VER}".patch.xz
 
 # kernel signatures
 gpg --auto-key-locate wkd --locate-keys torvalds@kernel.org gregkh@kernel.org
 
 
 # OSADL signing key for patch
-gpg  --recv-keys 514B0EDE3C387F944FB3799329E574109AEBFAAA
+gpg --keyserver pool.sks-keyservers.net \
+    --recv-keys 514B0EDE3C387F944FB3799329E574109AEBFAAA
 
-gpg --verify linux-${VERSION}.tar.sign
-gpg --verify patch-${VERSION}-${RT_PATCH_VER}.patch.sign
+gpg --verify linux-"${VERSION}".tar.sign
+gpg --verify patch-"${VERSION}"-"${RT_PATCH_VER}".patch.sign
 
-tar xvf linux-${VERSION}.tar
+tar xvf linux-"${VERSION}".tar
 
-cd linux-${VERSION}
+cd linux-"${VERSION}"
 
-cat ../patch-${VERSION}-${RT_PATCH_VER}.patch | patch -p1 --verbose
+patch -p1 --verbose < ../patch-"${VERSION}"-"${RT_PATCH_VER}".patch
 
 mv ../.config ./
